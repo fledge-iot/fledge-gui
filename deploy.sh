65fae0c1aa3d98170ffc4b1564eac50bb3b7dc1f
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2018-03-29 18:56:05 +0530

    added clean option in build.sh and updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,30 +15,69 @@ CINFO="${CPFX}1;32m"
 CWARN="${CPFX}0;33m"
 
 # Variables
-FOGLAMP_GUI_VER=1.2.0  # if not defined as -foglamp-version
+FOGLAMP_GUI_VER=1.2.0
+NGINX_STABLE_VER=1.12.2
+
+# Define Variable tecreset
+tecreset=$(tput sgr0)
+
+conection_details() {
+    # Check OS Type
+    os=$(uname -o)
+    echo -e "${CINFO}Operating System Type : ${CRESET}" $tecreset $os
+
+    # Check hostname
+    echo -e "${CINFO}Hostname :${CRESET}" $tecreset $HOSTNAME
+
+    # Check Internal IP
+    internalip=$(hostname -I)
+    echo -e "${CINFO}Internal IP :${CRESET}" $tecreset $internalip
+
+    # Check External IP
+    externalip=$(curl -s ipecho.net/plain;echo)
+
+    echo -e "${CINFO}External IP : $tecreset ${CRESET}"$externalip
+    echo     # new line
+}
+
+memory_footprints(){
+    # Check RAM and SWAP Usages
+    free -h | grep -v + > /tmp/ramcache
+    echo -e "${CINFO}Ram Usages :${CRESET}" $tecreset
+
+    cat /tmp/ramcache | grep -v "Swap"
+    echo -e "${CINFO}Swap Usages :${CRESET}" $tecreset
+    cat /tmp/ramcache | grep -v "Mem"
+    echo     # new line
+
+    # Check Disk Usages
+    df -h| grep 'Filesystem\|/dev/mmcblk0*' > /tmp/diskusage
+    echo -e "${CINFO}Disk Usages :${CRESET}" $tecreset 
+    cat /tmp/diskusage
+    echo     # new line
+}
 
 install () {
     if ! which nginx > /dev/null 2>&1; then
-        echo -e  WARNING: "${CWARN} WARNING: nginx not installed  ${CRESET}"
+        echo -e WARNING: "${CWARN} nginx not installed ${CRESET}"
         sudo apt-get install nginx-light
     else
         nginx_version=$(nginx -v 2>&1)
-        echo -e  INFO: "${CINFO} Found ${nginx_version} ${CRESET}"
+        echo -e INFO: "${CINFO} Found ${nginx_version} ${CRESET}"
     fi
     
+    echo -e INFO: "${CINFO} nginx status ${CRESET}"
+    sudo service nginx status
+    
+    sudo service nginx stop
+    sudo service nginx start
+    
     # download foglamp-gui build artifacts i.e. dist directory contents
-    # wget http://192.168.1.120/foglamp-gui-${FOGLAMP_GUI_VER}.tar.gz
-    tar -zxvf dist.tar.gz
+    wget http://192.168.1.120/foglamp-gui-${FOGLAMP_GUI_VER}.tar.gz
+    tar -zxvf foglamp-gui-${FOGLAMP_GUI_VER}.tar.gz
 
     # put them into /var/www/html and start nginx
     sudo mv dist/* /var/www/html/.
-
-    # check nginx is good
-    echo  -e  INFO: "${CINFO} nginx status  ${CRESET}"
-    sudo service nginx status
-
-    sudo service nginx stop
-    sudo service nginx start
 }
 
 ############################################################
@@ -68,7 +107,7 @@ EXAMPLES
 ############################################################
 execute_command() {
 
-  if [ "$OPTION" == "HELP" ]
+  if [[ "$OPTION" == "HELP" ]]
   then
     echo "${USAGE}"
 
@@ -76,8 +115,13 @@ execute_command() {
   then
     echo $__version__
 
-  fi
+  elif [ "$OPTION" == "START" ]
+  then
+    conection_details
+    memory_footprints
+    install
 
+  fi
 }
 
 ############################################################
@@ -95,13 +139,17 @@ then
       -v|--version)
         OPTION="VERSION"
       ;;
-      
+
+      -s|--start)
+        OPTION="START"
+      ;;
       *)
         echo "Unrecognized option: $i"
     esac
-
     execute_command
   done
 else
-  install
+  echo "${USAGE}"
+
 fi
+
