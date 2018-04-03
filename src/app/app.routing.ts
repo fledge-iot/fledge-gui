65ef8101ec9757fc7f71b3b9cb5f0dadab80e6de
Praveen Garg <praveen.garg@nerdapplabs.com>
2018-04-03 14:44:07 +0530

    fix 404 when manual url or refresh is tried with default nginx conf https://github.com/angular/angular-cli/issues/5113

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -37,4 +37,4 @@ const appRoutes: Routes = [
     { path: '**', redirectTo: '' }
 ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+export const routing = RouterModule.forRoot(appRoutes, {useHash:true});
