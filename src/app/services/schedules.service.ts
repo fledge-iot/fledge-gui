6ff3bbf5ddb76e3fd510f0ae82c751c6c1abcd7b
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2018-05-31 11:50:56 +0530

    fixed error not comming issue

--- src/app/services/schedules.service.ts
+++ src/app/services/schedules.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../environments/environment';
-
+import 'rxjs/add/observable/throw';
 @Injectable()
 export class SchedulesService {
 
