8f68e6d6a43197b30cc898c9622d72a2bdf3475f
ashish-jabble <ashish@dianomic.com>
2018-06-08 16:27:41 +0530

    Required for foglamp.deb package to install

--- raspbian/deploy-core.sh
+++ raspbian/deploy-core.sh
@@ -9,14 +9,15 @@ yes Y |  sudo apt-get install curl
 
 yes Y |  sudo apt-get install cmake g++ make build-essential autoconf automake
 yes Y |  sudo apt-get install libtool libboost-dev libboost-system-dev libboost-thread-dev libpq-dev
-yes Y |  sudo apt-get install python-dbus python-dev python3-pip
+yes Y |  sudo apt-get install python-setuptools python3-dbus python3-dev python3-pip
 
 yes Y |  sudo apt-get install bluez
 yes Y |  sudo apt-get install uuid-dev
+yes Y |  sudo apt-get install jq
 
 # default SQLite
 yes Y |  sudo apt-get install sqlite3 libsqlite3-dev
 
 # Required, until postgres depdendecies are plugged out
-yes Y |  sudo apt-get install postgresql
+yes Y |  sudo apt-get install postgresql postgresql-server-dev-9.6
 sudo -u postgres createuser -d pi
