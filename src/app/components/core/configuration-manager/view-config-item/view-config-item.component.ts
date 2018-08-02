162f00542d8c78b0badc0c74ec9c9ab915461ecf
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2018-08-02 17:38:42 +0530

    config validation (WIP)

--- src/app/components/core/configuration-manager/view-config-item/view-config-item.component.ts
+++ src/app/components/core/configuration-manager/view-config-item/view-config-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ConfigurationService, AlertService } from '../../../../services';
 import { NgProgress } from '../../../../../../node_modules/ngx-progressbar';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-view-config-item',
@@ -9,6 +10,7 @@ import { NgProgress } from '../../../../../../node_modules/ngx-progressbar';
 })
 export class ViewConfigItemComponent implements OnInit {
   @Input() categoryConfigurationData: any;
+  public oldValue;
 
   constructor(private configService: ConfigurationService,
     private alertService: AlertService,
@@ -16,30 +18,37 @@ export class ViewConfigItemComponent implements OnInit {
 
   ngOnInit() { }
 
-  public restoreConfigFieldValue(config_item_key: string) {
-    const inputField = <HTMLInputElement>document.getElementById(config_item_key.toLowerCase());
-    inputField.value = inputField.textContent;
-    const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + config_item_key.toLowerCase());
+  public restoreConfigFieldValue(configItemKey, categoryKey, form: NgForm) {
+    const itemKey = categoryKey.toLowerCase() + '-' + configItemKey.toLowerCase();
+    if (this.oldValue !== undefined) {
+      form.controls[itemKey].setValue(this.oldValue);
+    }
+    const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + itemKey);
+    cancelButton.classList.remove('active');
     cancelButton.classList.add('hidden');
   }
 
-  public saveConfigValue(category_name: string, config_item: string, type: string) {
-    const cat_item_id = (category_name.trim() + '-' + config_item.trim()).toLowerCase();
-    const inputField = <HTMLInputElement>document.getElementById(cat_item_id);
-    const value = inputField.value.trim();
-    const id = inputField.id.trim();
-    const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + id);
-    cancelButton.classList.add('hidden');
+  public saveConfigValue(categoryName, configItem, type, form: NgForm) {
+    const catItemId = categoryName + '-' + configItem;
+    // const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + id);
+    // cancelButton.classList.add('hidden');
+
+    console.log('form value ', form.controls[catItemId.toLowerCase()].value);
+    const value = form.controls[catItemId.toLowerCase()].value;
 
     /** request started */
     this.ngProgress.start();
-    this.configService.saveConfigItem(category_name, config_item, value, type).
+    this.configService.saveConfigItem(categoryName, configItem, value, type).
       subscribe(
         (data) => {
           /** request completed */
           this.ngProgress.done();
           if (data['value'] !== undefined) {
-            inputField.textContent = inputField.value = data['value'];
+            if (type.toUpperCase() === 'JSON') {
+              form.controls[catItemId.toLowerCase()].setValue(JSON.stringify(data['value']));
+            } else {
+              form.controls[catItemId.toLowerCase()].setValue(data['value']);
+            }
             this.alertService.success('Value updated successfully');
           }
         },
@@ -54,8 +63,52 @@ export class ViewConfigItemComponent implements OnInit {
         });
   }
 
-  public onTextChange(config_item_key: string) {
-    const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + config_item_key.toLowerCase());
+  public getConfigAttributeType(key) {
+    let type = '';
+    switch (key.trim().toUpperCase()) {
+      case 'STRING':
+      case 'IPV4':
+      case 'IPV6':
+      case 'X509 CERTIFICATE':
+      case 'PASSWORD':
+        type = 'TEXT';
+        break;
+      case 'INTEGER':
+        type = 'NUMBER';
+        break;
+      case 'BOOLEAN':
+        type = 'BOOLEAN';
+        break;
+      case 'JSON':
+      case 'X509 CERTIFICATE':
+        type = 'LONG_TEXT';
+        break;
+      default:
+        break;
+    }
+    return type;
+  }
+
+  onModelChange(oldVal) {
+    if (oldVal !== undefined) {
+      this.oldValue = oldVal;
+    }
+  }
+
+  public onTextChange(configItemKey: string, form: NgForm) {
+    // const previousCancelButton = <HTMLButtonElement>document.getElementsByClassName('active')[0];
+    // if (previousCancelButton !== undefined) {
+    //   previousCancelButton.classList.remove('active');
+    //   previousCancelButton.classList.add('hidden');
+    //   console.log(form.value);
+    //   if (this.oldValue !== undefined) {
+    //     form.controls[configItemKey.toLowerCase()].setValue(this.oldValue);
+    //   }
+    // }
+
+
+    const cancelButton = <HTMLButtonElement>document.getElementById('btn-cancel-' + configItemKey.toLowerCase());
+    cancelButton.classList.add('active');
     cancelButton.classList.remove('hidden');
   }
 }
