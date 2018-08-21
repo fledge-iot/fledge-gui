5de077540e8f947c2919a16b2826305f646f80ff
msharma <monika.sharma@nerdapplabs.com>
2018-08-21 20:30:40 +0530

    auto update north tasks

--- src/app/components/core/north/north.component.ts
+++ src/app/components/core/north/north.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NorthService, AlertService } from '../../../services/index';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { NorthService, AlertService, PingService } from '../../../services/index';
 import { Router } from '@angular/router';
 import { NorthTaskModalComponent } from './north-task-modal/north-task-modal.component';
 import { sortBy } from 'lodash';
+
+import { POLLING_INTERVAL } from '../../../utils';
+import { AnonymousSubscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-north',
   templateUrl: './north.component.html',
   styleUrls: ['./north.component.css']
 })
-export class NorthComponent implements OnInit {
+export class NorthComponent implements OnInit, OnDestroy {
   public task: string;
   public tasks: any;
 
-  constructor(private northService: NorthService, private alertService: AlertService, private router: Router) {}
+  public refreshInterval = POLLING_INTERVAL;
+  private timerSubscription: AnonymousSubscription;
+
+  constructor(private northService: NorthService, private ping: PingService, private alertService: AlertService, private router: Router) {}
 
   @ViewChild(NorthTaskModalComponent)
   northTaskModal: NorthTaskModalComponent;
 
   ngOnInit() {
     this.getNorthTasks();
+    this.ping.pingIntervalChanged.subscribe((timeInterval: number) => {
+      this.refreshInterval = timeInterval;
+    });
   }
 
   addNorthInstance() {
@@ -27,11 +37,18 @@ export class NorthComponent implements OnInit {
   }
 
   public getNorthTasks(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+      this.timerSubscription = null;
+    }
     this.northService.getNorthTasks().
       subscribe(
         (data) => {
           this.tasks = data;
           this.tasks = sortBy(this.tasks, ['enabled']).reverse();
+          if (this.refreshInterval > 0) {
+            this.enableRefreshTimer();
+          }
         },
         error => {
           if (error.status === 0) {
@@ -46,5 +63,17 @@ export class NorthComponent implements OnInit {
     this.task = task;
     // call child component method to toggle modal
     this.northTaskModal.toggleModal(true);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+      this.timerSubscription = null;
+    }
+  }
+
+  private enableRefreshTimer(): void {
+    this.timerSubscription = Observable.timer(this.refreshInterval)
+      .subscribe(() => this.getNorthTasks());
   }
 }
