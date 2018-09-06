331a78ed0be2d5101342fdc749fd5f9e096d4676
Praveen Garg <praveen.garg@nerdapplabs.com>
2018-09-07 01:34:13 +0530

    more fixes

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,8 +1,17 @@
-var webpack = require("webpack");
-module.exports = {
-  // ...
-  plugins: [
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
-    // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-  ]
-};
+const webpack = require("webpack");
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const webpackConfig = {
+    resolve: {
+      alias: {
+        'lodash-es': 'lodash', // our internal tests showed that lodash is a little bit smaller as lodash-es
+      }
+    },
+    plugins: [
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+        // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+        new LodashModuleReplacementPlugin()
+      ]
+}
+
+module.exports = webpackConfig
