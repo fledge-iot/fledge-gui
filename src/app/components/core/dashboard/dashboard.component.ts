0544a04b76fc0e903d1d6f026640220d8baf4266
Mohd Shariq <mohd.shariq@nerdapplabs.com>
2019-01-09 12:48:26 +0530

    removed extra import

--- src/app/components/core/dashboard/dashboard.component.ts
+++ src/app/components/core/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { map } from 'lodash';
-import { interval, Operator } from 'rxjs';
+import { interval } from 'rxjs';
 import 'rxjs/add/operator/takeWhile';
 
 import { DateFormatterPipe } from '../../../pipes';
