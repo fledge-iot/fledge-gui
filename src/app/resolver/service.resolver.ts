832d8e4aba8523596b5e54937d725f350338d80f
monika-Dianomic <monika@dianomic.com>
2021-02-08 12:01:28 +0530

    fixed console error on notification page

--- src/app/resolver/service.resolver.ts
+++ src/app/resolver/service.resolver.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ServicesApiService } from '../services';
 import { Resolve } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,10 @@ export class ServiceResolver implements Resolve<any> {
   constructor(private service: ServicesApiService) { }
 
   resolve() {
-    return this.service.getAllServices();
+    return this.service.getAllServices().pipe(
+      catchError(error => {
+        console.error(error);
+        return of(null);
+      }));
   }
-
 }
