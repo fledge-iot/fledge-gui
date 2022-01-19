ede4ad2f28c8e1f038eca815dac420273c098519
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-01-19 15:59:26 +0530

    removed unused components

--- src/app/components/core/control-dispatcher/control-dispatcher.module.ts
+++ src/app/components/core/control-dispatcher/control-dispatcher.module.ts
@@ -1,31 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ListControlDispatcherComponent } from './list-control-dispatcher/list-control-dispatcher.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ScriptDetailsComponent } from './script-details/script-details.component';
-import { WriteComponent } from './script-steps/write/write.component';
-import { OperationComponent } from './script-steps/operation/operation.component';
-import { ScriptComponent } from './script-steps/script/script.component';
-import { ConfigComponent } from './script-steps/config/config.component';
-import { PipesModule } from '../../../pipes/pipes.module';
-import { ControlScriptsListComponent } from './list-control-dispatcher/control-scripts-list/control-scripts-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { TreeModule } from '@circlon/angular-tree-component';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { DelayComponent } from './script-steps/delay/delay.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AddControlScriptComponent } from './add-control-script/add-control-script.component';
-import { AddStepComponent } from './add-control-script/add-step/add-step.component';
-import { AddWriteComponent } from './add-control-script/add-step/add-write/add-write.component';
-import { AddStepConditionComponent } from './add-control-script/add-step/add-step-condition/add-step-condition.component';
-import { AddStepValueComponent } from './add-control-script/add-step/add-step-value/add-step-value.component';
-import { AddOperationComponent } from './add-control-script/add-step/add-operation/add-operation.component';
-import { AddScriptComponent } from './add-control-script/add-step/add-script/add-script.component';
-import { AddConfigureComponent } from './add-control-script/add-step/add-configure/add-configure.component';
-import { AddDelayComponent } from './add-control-script/add-step/add-delay/add-delay.component';
-import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 import { DirectivesModule } from '../../../directives/directives.module';
 import { AdminGuard, AuthGuard } from '../../../guards';
+import { PipesModule } from '../../../pipes/pipes.module';
+import { AddControlScriptComponent } from './add-control-script/add-control-script.component';
+import { AddConfigureComponent } from './add-control-script/add-step/add-configure/add-configure.component';
+import { AddDelayComponent } from './add-control-script/add-step/add-delay/add-delay.component';
+import { AddOperationComponent } from './add-control-script/add-step/add-operation/add-operation.component';
+import { AddScriptComponent } from './add-control-script/add-step/add-script/add-script.component';
+import { AddStepConditionComponent } from './add-control-script/add-step/add-step-condition/add-step-condition.component';
+import { AddStepValueComponent } from './add-control-script/add-step/add-step-value/add-step-value.component';
+import { AddStepComponent } from './add-control-script/add-step/add-step.component';
+import { AddWriteComponent } from './add-control-script/add-step/add-write/add-write.component';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+import { ControlScriptsListComponent } from './list-control-dispatcher/control-scripts-list/control-scripts-list.component';
+import { ListControlDispatcherComponent } from './list-control-dispatcher/list-control-dispatcher.component';
 
 const routes: Routes = [
 
@@ -40,7 +34,6 @@ const routes: Routes = [
   },
   {
     path: 'script/:name',
-    // component: ScriptDetailsComponent
     component: AddControlScriptComponent
   }
 ];
@@ -48,13 +41,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     ListControlDispatcherComponent,
-    ScriptDetailsComponent,
-    WriteComponent,
-    OperationComponent,
-    ScriptComponent,
-    ConfigComponent,
     ControlScriptsListComponent,
-    DelayComponent,
     AddControlScriptComponent,
     AddStepComponent,
     AddWriteComponent,
