bac9cf4a42771cc1c75dd3d01a97cf410ba21590
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-02-14 12:12:48 +0530

    changed git version show in setting page

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,7 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
-import { environment } from '../../../../environments/environment';
 import * as data from '../../../../git-version.json';
 import * as moment from 'moment';
 
@@ -11,10 +9,22 @@ import * as moment from 'moment';
   styleUrls: ['./footer.component.css']
 })
 
-export class FooterComponent {
-  public appVersion: string = environment.VERSION;
+export class FooterComponent implements OnInit {
+  public appVersion;
   public git = data['default'];
   copyrightYear = moment().year();
 
-  constructor(public router: Router) {}
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+    console.log('git', this.git);
+    console.log('distance', this.git.distance);
+    console.log('raw', this.git.raw);
+    if (this.git?.distance == 0) {
+      // released version
+      this.appVersion = `${this.git.version}-${this.git.hash}`
+    } else {
+      this.appVersion = this.git.raw.substr(0, this.git.raw.lastIndexOf('-'));
+    }
+  }
 }
