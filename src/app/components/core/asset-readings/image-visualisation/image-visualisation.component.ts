21454afc506f46be3d32c7a8c8556b8c4f30ed2d
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-03-02 18:06:23 +0530

    8 bit image visualization
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/components/core/asset-readings/image-visualisation/image-visualisation.component.ts
+++ src/app/components/core/asset-readings/image-visualisation/image-visualisation.component.ts
@@ -1,6 +1,4 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { decode, encode } from 'base64-arraybuffer';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AssetsService } from '../../../../services';
@@ -15,11 +13,10 @@ export class ImageVisualisationComponent implements OnInit {
   assetCode = '';
   destroy$: Subject<boolean> = new Subject<boolean>();
   imageBase64String = '';
-  imageUrl;
+  image;
 
   constructor(
-    public assetService: AssetsService,
-    private domSanitizer: DomSanitizer) { }
+    public assetService: AssetsService) { }
 
   ngOnInit(): void { }
 
@@ -44,29 +41,79 @@ export class ImageVisualisationComponent implements OnInit {
   getAssetReadings(assetCode) {
     this.assetService.getLatestReadings(assetCode)
       .pipe(takeUntil(this.destroy$))
-      .subscribe(
-        (data: any) => {
-          // TO TEST INITIAL IMAGE LOADING
-          const base64 = data[0].reading['testcard'].split('8_')[1];
-          var bytes = Uint8Array.from(atob(base64), c => c.charCodeAt(0))
-          // var binary_string = window.atob(base64);
-          // var len = binary_string.length;
-          // var bytes = new Uint8Array(len);
-          // for (var i = 0; i < len; i++) {
-          //   bytes[i] = binary_string.charCodeAt(i);
-          // }
-          // console.log('base64', base64String);
-          // const dataArray = decode(base64String);
-          // const byte = new Uint8Array(dataArray);
-          // console.log('bytes array', byte.buffer);
-          const blob = new Blob([bytes.buffer], { type: 'image/png' });
-          const imageUrl = URL.createObjectURL(blob);
-          this.imageUrl = this.domSanitizer.bypassSecurityTrustUrl(imageUrl);
-          console.log('img', this.imageUrl);
-        },
+      .subscribe((data: any) => {
+        // Check if returned readings are of image type
+        if (data.length === 0) {
+          console.log('No readings found.');
+
+          return;
+        }
+        var imageExists = Object.keys(data[0].reading).some(function (k) {
+          return typeof (data[0].reading[k]) === 'string' && data[0].reading[k].includes("__DPIMAGE");
+        });
+        if (imageExists) {
+          // split image data
+          const imageData = data[0].reading[assetCode].replace('__DPIMAGE:', '').split('_');
+
+          // Get base64 raw string
+          const base64Str_ = imageData[1];
+
+          // Get width, height and depth of the image and convert values into Number
+          const [width, height, depth] = imageData[0].split(',').map(Number);
+
+          let arrayBufferView = null;
+          if (depth === 8) {
+            arrayBufferView = Uint8Array.from(atob(base64Str_), c => c.charCodeAt(0))
+          } else if (depth === 16) {
+            // FIX ME! test 16 bit raw image array
+            // arrayBufferView = Uint16Array.from(atob(base64Str_), c => c.charCodeAt(0));
+          } else if (depth === 24) {
+            // FIX ME! test 24 bit raw image array
+            // arrayBufferView = Uint8Array.from(atob(base64Str_), c => c.charCodeAt(0));
+          } else {
+            console.log(`Not supported, found ${depth}`);
+          }
+
+          this.processImage(arrayBufferView, { width, height, depth });
+        }
+      },
         error => {
           console.log('error in response', error);
         });
+
+  }
+
+  processImage(buffer, options: any = {}) {
+    let view = null;
+    let out = null;
+
+    if (options.depth === 8) {
+      view = new Uint8Array(buffer);
+      out = new Uint8ClampedArray(buffer.byteLength * 4);
+      // set alpha channel
+      view.forEach((a, i) => out[(i * 4) + 3] = a);
+    }
+
+    // FIX ME
+    if (options.depth === 16) {
+      // view = new Uint16Array(buffer);
+      // out = new Uint8ClampedArray(buffer.byteLength * 2);
+      // // set alpha channel
+      // view.forEach((a, i) => out[(i * 4) + 3] = a);
+    }
+
+    console.log('view', view.length);
+    console.log('out', out.length);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = options.width;
+    canvas.height = options.height;
+
+    const image = new ImageData(out, options.width, options.height)
+    console.log('image', image);
+    canvas.getContext('2d').putImageData(image, 0, 0);
+    // if you want to save a png version
+    this.image = canvas.toDataURL("image/png");
   }
 
   public ngOnDestroy(): void {
