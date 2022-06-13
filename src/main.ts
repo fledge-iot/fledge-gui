41864e88fed5c507ffad755c70441dc2619cf163
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-06-13 18:27:26 +0530

    removed extra code block

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,3 @@
 import('./bootstrap')
   .catch(err => console.error(err));
 
-// import { loadRemoteEntry } from '@angular-architects/module-federation';
-
-// fetch('./assets/json/mfe-routes.json').then(res => res.json())
-//   .then(jsonData => {
-//     const promises: any[] = [];
-//     jsonData.forEach((mfe: any) => {
-//       promises.push(loadRemoteEntry({ type: mfe.type, remoteEntry: mfe.remoteEntry }))
-//     })
-//     Promise.all(promises)
-//       .catch(err => console.error('Error loading remote entries', err))
-//       .then(() => {
-//         import('./bootstrap')
-//       })
-//       .catch(err => console.error('got error', err));
-//   }).catch(err => {
-//     console.error('json load error', err)
-//     import('./bootstrap');
-//   });
-
