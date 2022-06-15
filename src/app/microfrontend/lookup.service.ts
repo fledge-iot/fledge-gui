5bf98b6480da1c6b2d784e030cb5f23e18e1541c
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-06-15 14:28:30 +0530

    folder name changed from json to routes

--- src/app/microfrontend/lookup.service.ts
+++ src/app/microfrontend/lookup.service.ts
@@ -4,7 +4,7 @@ import { Microfrontend } from './microfrontend';
 @Injectable({ providedIn: 'root' })
 export class LookupService {
   lookup(): Promise<Microfrontend[]> {
-    return fetch('./assets/json/mfe-routes.json').then(res => res.json())
+    return fetch('./assets/routes/default-routes.json').then(res => res.json())
       .then(jsonData => {
         return Promise.resolve(jsonData);
       }).catch(() => {
