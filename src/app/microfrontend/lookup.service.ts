61df358f52b00ba5d59925fba0c60b37c8d313b2
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2022-07-15 16:13:54 +0530

    access mfe in dev mode

--- src/app/microfrontend/lookup.service.ts
+++ src/app/microfrontend/lookup.service.ts
@@ -1,39 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Microfrontend } from './microfrontend';
 import { flatten } from 'lodash';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class LookupService {
   async lookup(): Promise<Microfrontend[]> {
-    const routes = await this.readRoutes();
-    let routesFilePathArray = [];
-    routes.forEach(el => {
-      routesFilePathArray.push(fetch(`./${el}/assets/routes.json`));
-    });
+    if (!environment.production) {
+      return fetch('./assets/routes.json').then(res => res.json())
+        .then(jsonData => {
+          return Promise.resolve(jsonData);
+        }).catch(() => {
+          // console.error('json load error', err);
+          return Promise.resolve([]);
+        });
+    } else {
+      const routes = await this.readRoutes();
+      let routesFilePathArray = [];
+      routes.forEach(el => {
+        routesFilePathArray.push(fetch(`./${el}/assets/routes.json`));
+      });
 
-    /**
-     * Takes an iterable of promises i.e.routesFilePathArray as an input,
-     * and returns a single Promise that resolves to an array of the results
-     * of the input promises. This returned promise will
-     * resolve when all of the input's promises have resolved,
-     * or if the input iterable contains no promises.
-     * It rejects immediately upon any of the input promises rejecting or non-promises throwing an error,
-     * and will reject with this first rejection message / error.
-     */
-    return Promise.all(routesFilePathArray)
       /**
-       * The Promise.allSettled() method returns a promise that resolves after
-       * all of the given promises have either fulfilled or rejected,
-       * with an array of objects that each describes the outcome of each promise.
+       * Takes an iterable of promises i.e.routesFilePathArray as an input,
+       * and returns a single Promise that resolves to an array of the results
+       * of the input promises. This returned promise will
+       * resolve when all of the input's promises have resolved,
+       * or if the input iterable contains no promises.
+       * It rejects immediately upon any of the input promises rejecting or non-promises throwing an error,
+       * and will reject with this first rejection message / error.
        */
-      .then(results => Promise.allSettled(results.map(r => r.json())))
-      .then((results) => {
-        const response = results.filter(x => x.status === "fulfilled").map((x: any) => x.value);
-        return Promise.resolve(flatten(response));
-      })
-      .catch(() => {
-        return Promise.resolve([]);
-      })
+      return Promise.all(routesFilePathArray)
+        /**
+         * The Promise.allSettled() method returns a promise that resolves after
+         * all of the given promises have either fulfilled or rejected,
+         * with an array of objects that each describes the outcome of each promise.
+         */
+        .then(results => Promise.allSettled(results.map(r => r.json())))
+        .then((results) => {
+          const response = results.filter(x => x.status === "fulfilled").map((x: any) => x.value);
+          return Promise.resolve(flatten(response));
+        })
+        .catch(() => {
+          return Promise.resolve([]);
+        });
+    }
   }
 
   async readRoutes(): Promise<string[]> {
