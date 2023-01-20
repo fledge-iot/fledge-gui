917a31c1f5fbeca67b0888071b10922a5d1962d2
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2023-01-20 14:53:26 +0530

    double quote empty string check in file content when creating file content
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/components/core/configuration-manager/view-config-item/view-config-item.component.ts
+++ src/app/components/core/configuration-manager/view-config-item/view-config-item.component.ts
@@ -215,7 +215,8 @@ export class ViewConfigItemComponent implements OnInit, OnChanges, OnDestroy {
     }) : [];
 
     this.filesToUpload = changedConfigValues.map((d) => {
-      if (d.type === 'script') {
+      // validate empty value
+      if (d.type === 'script' && !(/^(""|''|)$/.test(d.value))) {
         return this.createFileToUpload(d);
       }
     }).filter(f => f !== undefined);
@@ -342,6 +343,8 @@ export class ViewConfigItemComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   public uploadScript() {
+    console.log(this.filesToUpload);
+
     this.filesToUpload.forEach(data => {
       let configItem: any;
       configItem = Object.keys(data)[0];
