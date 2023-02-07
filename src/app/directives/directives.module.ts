42d62d247a046819354002fa17498a41a18406cb
monika-Dianomic <monika@dianomic>
2023-02-07 14:41:57 +0530

    updated the code by using pattern instead of directive

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -7,14 +7,13 @@ import { EqualValidatorDirective } from './equal-validator.directive';
 import { InputMaskDirective } from './input-mask.directive';
 import { InputTrimDirective } from './input-trim.directive';
 import { NumberOnlyDirective } from './number-only.directive';
-import { RestrictSpecialCharDirective } from './restrict-special-char.directive';
 import { AccessControlDirective } from './access-control.directive';
 
 @NgModule({
   imports: [],
-  declarations: [NumberOnlyDirective, RestrictSpecialCharDirective, CustomMinDirective, EqualValidatorDirective, InputMaskDirective,
+  declarations: [NumberOnlyDirective, CustomMinDirective, EqualValidatorDirective, InputMaskDirective,
     InputTrimDirective, CustomMinDirective, CustomMaxDirective, CustomJsonDirective, AccessControlDirective],
-  exports: [NumberOnlyDirective, RestrictSpecialCharDirective, EqualValidatorDirective, InputMaskDirective, InputTrimDirective,
+  exports: [NumberOnlyDirective, EqualValidatorDirective, InputMaskDirective, InputTrimDirective,
     CustomMinDirective, CustomMaxDirective, CustomJsonDirective, AccessControlDirective]
 })
 export class DirectivesModule { }
