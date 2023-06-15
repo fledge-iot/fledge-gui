86b05a07b9773528eb396afda74de0d077c04dc1
monika-Dianomic <monika@dianomic.com>
2023-06-15 15:19:30 +0530

    show red underline for invalid Advanced/Security tab

--- src/app/components/core/configuration-manager/configuration-group/configuration-group.component.ts
+++ src/app/components/core/configuration-manager/configuration-group/configuration-group.component.ts
@@ -243,9 +243,15 @@ export class ConfigurationGroupComponent implements AfterViewInit {
   }
 
   formStatus(formState: any) {
-    // find the object of changed form from groups array and set its status
+    // find the object of changed form from groups array
     const groupObject = this.groups.find((g: any) => g.group === formState.group);
-    groupObject.status = formState.status;
+
+    // update the status of respected form
+    if (groupObject) {
+      groupObject.status = formState.status;
+    } else if (['Advanced Configuration', 'Security Configuration'].includes(formState.group)) {
+      this.advanceCategoriesGroup.find((g: any) => g.group === formState.group).status = formState.status;
+    }
 
     // check the condition for every element to see if all groups have valid status
     const formStatus = this.groups.every(g => (g.status === true || g.status === undefined));
