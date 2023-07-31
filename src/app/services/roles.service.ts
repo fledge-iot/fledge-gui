9339f3bc693e2ca727aa640cec36c30d59a4aeb2
Mohd Shariq <mohd.shariq@nerdapplabs.com>
2023-07-31 16:59:23 +0530

    feedback fix
    Signed-off-by: Mohd Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/services/roles.service.ts
+++ src/app/services/roles.service.ts
@@ -24,13 +24,12 @@ export class RolesService {
     return [appRoles.admin, appRoles.control, appRoles.anonymous].includes(roleId);
   }
 
-  editPermissionBaseOnPageAccess(fromPage: string) {
-    if (fromPage == 'control-pipeline' && this.hasControlAccess()) {
-      return true;
-    } else if (fromPage !== 'control-pipeline' && this.hasEditPermissions()) {
-      return true;
+  editPermissionBaseOnPageAccess(page: string) {
+    if (page == 'control-pipeline') {
+      return this.hasControlAccess();
+    } else {
+      return this.hasEditPermissions()
     }
-    return false;
   }
 
   /**
