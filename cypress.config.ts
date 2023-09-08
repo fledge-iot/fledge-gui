302f138f37cc0b6881b19b36bc3aca9d95bb786b
hemant <hemantgarg923@gmail.com>
2023-09-08 18:08:37 +0530

    south test suite files migrated to cypress syntax
    Signed-off-by: hemant <hemantgarg923@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,44 @@ export default defineConfig({
   
   e2e: {
     'baseUrl': 'http://localhost:4200',
-    supportFile: false
+    supportFile: false,
+    specPattern: [
+      './e2e/**/*.e2e-*.ts'
+    ]
   },
+  
+
+  defaultCommandTimeout: 11000,
+  // suites: {
+  //   common: './e2e/specs/**/*.e2e-common.ts',
+  //   south: './e2e/specs/**/*.e2e-south.ts',
+  // },
+  // capabilities: {
+  //   'browserName': 'chrome'
+  // },
+  // directConnect: true,
+  // framework: 'jasmine',
+  // jasmineNodeOpts: {
+  //   showColors: true,
+  //   defaultTimeoutInterval: 30000,
+  //   print: function () { }
+  // },
+  // onPrepare() {
+  //   var HtmlReporter = require('protractor-beautiful-reporter');
+  //   // Add a screenshot reporter and store screenshots to `/e2e-test-report`:
+  //     jasmine.getEnv().addReporter(new HtmlReporter({
+  //        baseDirectory: 'e2e-test-report',
+  //        jsonsSubfolder: 'json',
+  //        screenshotsSubfolder: 'screenshots',
+  //        takeScreenShotsOnlyForFailedSpecs: true,
+  //        preserveDirectory: false
+  //     }).getJasmine2Reporter());
+
+  //   require('ts-node').register({
+  //     project: 'e2e/tsconfig.e2e.json'
+  //   });
+  //   jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  // }
   
   
 })
--- e2e/specs/app.e2e-south.ts
+++ e2e/specs/app.e2e-south.ts
@@ -1,32 +1,32 @@
-import { SouthPage } from '../po/south.page';
-import { Filters } from '../po/app.filters';
+import { SouthPage } from '../po/south.page'
+import { Filters } from '../po/app.filters'
 
 describe('Fledge South Page tests', () => {
-  const southPage = new SouthPage();
-  const filters = new Filters();
+  const southPage = new SouthPage()
+  const filters = new Filters()
 
   it('Should display south service page', () => {
-    southPage.navToSouthPage();
-    expect(southPage.getSouthPageTitle()).toEqual('South Services');
-  });
+    southPage.navToSouthPage()
+    expect(southPage.getSouthPageTitle()).to.equal('South Services')
+  })
 
   it('Should display added south service', () => {
-    southPage.clickAddServiceButton();
-    southPage.addSouthService('guiE2eService #1'); // pass south service name
-    expect(southPage.getServiceName()).toEqual('guiE2eService #1');
-  });
+    southPage.clickAddServiceButton()
+    southPage.addSouthService('guiE2eService #1') // pass south service name
+    expect(southPage.getServiceName()).to.equal('guiE2eService #1')
+  })
 
   it('Should display added filter in south service', () => {
-    southPage.openSouthServiceModal();
-    filters.openFilterWizard();
-    filters.addFilter('guiE2eFilter #1');
-    expect(filters.getAddedFilterName()).toEqual(' guiE2eFilter #1');
-    southPage.closeSouthServiceModal();
-  });
+    southPage.openSouthServiceModal()
+    filters.openFilterWizard()
+    filters.addFilter('guiE2eFilter #1')
+    expect(filters.getAddedFilterName()).to.equal(' guiE2eFilter #1')
+    southPage.closeSouthServiceModal()
+  })
 
   it('Should display asset count on south service', () => {
-    southPage.getAssetCount().then(value => {
-      expect(+value).toBeGreaterThanOrEqual(1);
-    });
-  });
-});
+    southPage.getAssetCount().then((value) => {
+      assert.isAtLeast(+value, 1)
+    })
+  })
+})
