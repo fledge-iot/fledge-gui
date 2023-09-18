c31f0853831fa426ff772cc10bc3e5dcf61a204a
hemant <hemantgarg923@gmail.com>
2023-09-18 17:06:48 +0530

    mochawesome reporter added to create html report file
    Signed-off-by: hemant <hemantgarg923@gmail.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     'baseUrl': 'http://localhost:4200',
     supportFile: false,
     specPattern: [
       './e2e/**/*.e2e-*.ts'
-    ]
+    ],
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 
   viewportWidth: 1600,
