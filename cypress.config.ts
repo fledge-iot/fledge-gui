6591817c2653cee032697c38e76121fe4089705f
Hemant Kumar <81292944+hemantgarg923@users.noreply.github.com>
2023-09-19 17:55:35 +0530

    FOGL-8062.ci: E2E configurations updated to support cypress framework (#326)
    * e2e configurations updated to support cypress framework
    
    Signed-off-by: Praveen Garg <praveen.garg@nerdapplabs.com>
    Signed-off-by: hemant <hemantgarg923@gmail.com>
    Co-authored-by: Praveen Garg <praveen.garg@nerdapplabs.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'e2e-test-report'
+  },
   e2e: {
     'baseUrl': 'http://localhost:4200',
     supportFile: false,
     specPattern: [
       './e2e/**/*.e2e-*.ts'
-    ]
+    ],
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
   },
 
   viewportWidth: 1600,
