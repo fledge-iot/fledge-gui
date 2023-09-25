9bb2c85862998e38ae3c9d9f55cea74d2e181f53
monika-Dianomic <monika@dianomic.com>
2023-09-25 16:38:25 +0530

    changed in design of tree structure on configuration page

--- src/app/components/core/configuration-manager/configuration-manager.component.ts
+++ src/app/components/core/configuration-manager/configuration-manager.component.ts
@@ -40,92 +40,140 @@ export class ConfigurationManagerComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getRootCategories(true);
+    // this.getRootCategories(true);
+    this.getTreeStructure();
   }
 
-  public getRootCategories(onLoadingPage = false) {
-    this.rootCategories = [];
-    this.configService.getRootCategories().
-      subscribe(
-        (data) => {
-          data['categories'].forEach(element => {
-            if (element.hasOwnProperty('displayName')) {
-              this.rootCategories.push({
-                key: element.key,
-                displayName: element.displayName,
-                description: element.description
-              });
-            } else {
-              this.rootCategories.push({
-                key: element.key,
-                description: element.description
-              });
-            }
-            this.rootCategories = this.rootCategories.filter(el => el.key.toUpperCase() !== 'SOUTH')
-              .filter(el => el.key.toUpperCase() !== 'NORTH')
-              .filter(el => el.key.toUpperCase() !== 'NOTIFICATIONS');
-          });
-          if (onLoadingPage === true) {
-            this.getChildren(this.selectedRootCategory);
-          }
-        },
-        error => {
-          if (error.status === 0) {
-            console.log('service down ', error);
-          } else {
-            this.alertService.error(error.statusText);
-          }
-        });
-  }
+  // public getRootCategories(onLoadingPage = false) {
+  //   this.rootCategories = [];
+  //   this.configService.getRootCategories().
+  //     subscribe(
+  //       (data) => {
+  //         data['categories'].forEach(element => {
+  //           if (element.hasOwnProperty('displayName')) {
+  //             console.log('element1', element);
+  //             this.rootCategories.push({
+  //               key: element.key,
+  //               displayName: element.displayName,
+  //               description: element.description
+  //             });
+  //           } else {
+  //             this.rootCategories.push({
+  //               key: element.key,
+  //               description: element.description
+  //             });
+  //           }
+  //           this.rootCategories = this.rootCategories.filter(el => el.key.toUpperCase() !== 'SOUTH')
+  //             .filter(el => el.key.toUpperCase() !== 'NORTH')
+  //             .filter(el => el.key.toUpperCase() !== 'NOTIFICATIONS');
+  //         });
+  //         if (onLoadingPage === true) {
+  //           this.getChildren(this.selectedRootCategory);
+  //         }
+  //       },
+  //       error => {
+  //         if (error.status === 0) {
+  //           console.log('service down ', error);
+  //         } else {
+  //           this.alertService.error(error.statusText);
+  //         }
+  //       });
+  // }
 
-  getSelectedCategoryConfig(rootCategory: any) {
-    this.selectedRootCategory = this.hasProperty(rootCategory, 'displayName') === true ?
-      rootCategory.displayName : rootCategory.key;
-    this.getChildren(rootCategory.key);
-  }
+  // getSelectedCategoryConfig(rootCategory: any) {
+  //   this.selectedRootCategory = this.hasProperty(rootCategory, 'displayName') === true ?
+  //     rootCategory.displayName : rootCategory.key;
+  //   this.getChildren(rootCategory.key);
+  // }
 
-  public getChildren(categoryName) {
+  // public getChildren(categoryName) {
+  //   /** request started */
+  //   this.ngProgress.start();
+  //   this.tree.treeModel.nodes = [];
+
+  //   this.nodes = [];
+  //   this.configService.getChildren(categoryName).
+  //     subscribe(
+  //       (data) => {
+  //         /** request completed */
+  //         this.ngProgress.done();
+  //         const rootCategories = this.rootCategories.filter(el => el.key === categoryName);
+
+  //         // Check if there is any category
+  //         if (rootCategories.length > 0 && data['categories'].length === 0) {
+  //           this.isChild = false;
+  //           this.getCategory(rootCategories[0].key, rootCategories[0].description);
+  //           this.categoryData = [];
+  //           return;
+  //         }
+  //         this.isChild = true;
+  //         data['categories'].forEach(element => {
+  //           if (element.hasOwnProperty('displayName')) {
+  //             console.log('element22', element);
+  //             this.nodes.push({
+  //               id: element.key,
+  //               name: element.displayName,
+  //               description: element.description,
+  //               hasChildren: true, children: []
+  //             });
+  //           } else {
+  //             this.nodes.push({
+  //               id: element.key,
+  //               name: element.description,
+  //               description: element.description,
+  //               hasChildren: true, children: []
+  //             });
+  //           }
+  //         });
+
+  //         this.tree.treeModel.update();
+  //         if (this.tree.treeModel.getFirstRoot()) {
+  //           this.tree.treeModel.getFirstRoot().setIsActive(true);
+  //         }
+  //       },
+  //       error => {
+  //         /** request completed */
+  //         this.ngProgress.done();
+  //         if (error.status === 0) {
+  //           console.log('service down ', error);
+  //         } else {
+  //           this.alertService.error(error.statusText);
+  //         }
+  //       });
+  // }
+
+  public getTreeStructure() {
     /** request started */
     this.ngProgress.start();
     this.tree.treeModel.nodes = [];
 
     this.nodes = [];
-    this.configService.getChildren(categoryName).
+    this.configService.getRootCategories().
       subscribe(
         (data) => {
           /** request completed */
           this.ngProgress.done();
-          const rootCategories = this.rootCategories.filter(el => el.key === categoryName);
 
           // Check if there is any category
-          if (rootCategories.length > 0 && data['categories'].length === 0) {
+          if (data['categories'].length === 0) {
             this.isChild = false;
-            this.getCategory(rootCategories[0].key, rootCategories[0].description);
             this.categoryData = [];
             return;
           }
           this.isChild = true;
           data['categories'].forEach(element => {
-            if (element.hasOwnProperty('displayName')) {
-              this.nodes.push({
-                id: element.key,
-                name: element.displayName,
-                description: element.description,
-                hasChildren: true, children: []
-              });
-            } else {
-              this.nodes.push({
-                id: element.key,
-                name: element.description,
-                description: element.description,
-                hasChildren: true, children: []
-              });
-            }
+            this.nodes.push({
+              id: element.key,
+              name: (element.hasOwnProperty('displayName')) ? element.displayName : element.description,
+              description: element.description,
+              hasChildren: (element.children.length) > 0 ? true : false , children: this.addNameKey(element.children)
+            });
           });
-
+          this.getCategory(data['categories'][0]['children'][0].key, data['categories'][0]['children'][0].description);
           this.tree.treeModel.update();
-          if (this.tree.treeModel.getFirstRoot()) {
-            this.tree.treeModel.getFirstRoot().setIsActive(true);
+          if (this.tree.treeModel.getFirstRoot()) {          
+            const firstRootChild = this.tree.treeModel.getNodeById(data['categories'][0]['children'][0].id);
+            firstRootChild.setActiveAndVisible();
           }
         },
         error => {
@@ -139,41 +187,54 @@ export class ConfigurationManagerComponent implements OnInit {
         });
   }
 
-  public onNodeToggleExpanded(event) {
-    event.node.data.children = [];
-    if (event.node.isExpanded) {
-      this.configService.getChildren(event.node.data.id).
-        subscribe(
-          (data) => {
-            data['categories'].forEach(element => {
-              if (element.hasOwnProperty('displayName')) {
-                event.node.data.children.push({
-                  id: element.key,
-                  name: element.displayName,
-                  description: element.description,
-                  hasChildren: true,
-                  children: []
-                });
-              } else {
-                event.node.data.children.push({
-                  id: element.key,
-                  name: element.description,
-                  description: element.description,
-                  hasChildren: true,
-                  children: []
-                });
-              }
-              this.tree.treeModel.update();
-            });
-          }, error => {
-            if (error.status === 0) {
-              console.log('service down ', error);
-            } else {
-              this.alertService.error(error.statusText);
-            }
-          });
-    }
+  addNameKey(childrenData) {
+    Object.keys(childrenData).forEach((k) => {
+      childrenData[k]['name'] = childrenData[k]['displayName'];
+      childrenData[k]['id'] = childrenData[k]['key'];
+      delete childrenData[k]['displayName'];
+      if (childrenData[k]['children']) {
+        this.addNameKey(childrenData[k]['children']);
+      }  
+    });
+    return childrenData;
   }
+
+  // public onNodeToggleExpanded(event) {
+  //   event.node.data.children = [];
+  //   if (event.node.isExpanded) {
+  //     this.configService.getChildren(event.node.data.id).
+  //       subscribe(
+  //         (data) => {
+  //           data['categories'].forEach(element => {
+  //             console.log('el', element);
+  //             if (element.hasOwnProperty('displayName')) {
+  //               event.node.data.children.push({
+  //                 id: element.key,
+  //                 name: element.displayName,
+  //                 description: element.description,
+  //                 hasChildren: element.children.length,
+  //                 children: element.children
+  //               });
+  //             } else {
+  //               event.node.data.children.push({
+  //                 id: element.key,
+  //                 name: element.description,
+  //                 description: element.description,
+  //                 hasChildren: element.children.length,
+  //                 children: element.children
+  //               });
+  //             }
+  //             this.tree.treeModel.update();
+  //           });
+  //         }, error => {
+  //           if (error.status === 0) {
+  //             console.log('service down ', error);
+  //           } else {
+  //             this.alertService.error(error.statusText);
+  //           }
+  //         });
+  //   }
+  // }
 
   public onNodeActive(event) {
     this.getCategory(event.node.data.id, event.node.data.description);
@@ -184,10 +245,10 @@ export class ConfigurationManagerComponent implements OnInit {
     dropDown.classList.toggle('is-active');
   }
 
-  public resetAllFilters() {
-    this.selectedRootCategory = 'General';
-    this.getRootCategories(true);
-  }
+  // public resetAllFilters() {
+  //   this.selectedRootCategory = 'General';
+  //   this.getRootCategories(true);
+  // }
 
   private getCategory(categoryKey: string, categoryDesc: string): void {
     /** request started */
