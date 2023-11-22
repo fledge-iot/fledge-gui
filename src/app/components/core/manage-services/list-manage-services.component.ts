988740ed9af5565bc2a66bbd2e89a87002a8120a
monika-Dianomic <monika@dianomic.com>
2023-11-22 16:30:24 +0530

    initial design of manage services page

--- src/app/components/core/manage-services/list-manage-services.component.ts
+++ src/app/components/core/manage-services/list-manage-services.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AlertService, ProgressBarService, RolesService } from '../../../services';
+import { AlertService, ProgressBarService, RolesService, ServicesApiService } from '../../../services';
 import { SharedService } from '../../../services/shared.service';
 
 @Component({
@@ -11,27 +11,49 @@ import { SharedService } from '../../../services/shared.service';
 export class ListManageServicesComponent implements OnInit {
   private viewPortSubscription: Subscription;
   viewPort: any = '';
+  services = ['Notification Service', 'Dispatcher Service', 'Bucket Storage'];
+  installedServices;
 
   constructor(
     public sharedService: SharedService,
     private alertService: AlertService,
     public ngProgress: ProgressBarService,
+    public servicesApiService: ServicesApiService,
     public rolesService: RolesService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.viewPortSubscription = this.sharedService.viewport
       .subscribe(viewport => {
         this.viewPort = viewport;
       });
-    this.getAllServices();
+      await this.getInstalledServicesList();
   }
 
-  getAllServices() {
-    console.log('getAllServices');
+  addService() {
+    console.log('addService');
+  }
+
+  public async getInstalledServicesList() {
+    /** request start */
+    this.ngProgress.start();
+    await this.servicesApiService.getInstalledServices().
+      then(data => {
+        /** request done */
+        this.ngProgress.done();
+        this.installedServices = data['services'];
+      })
+      .catch(error => {
+        /** request done */
+        this.ngProgress.done();
+        if (error.status === 0) {
+          console.log('service down ', error);
+        } else {
+          this.alertService.error(error.statusText);
+        }
+      });
   }
 
   public ngOnDestroy(): void {
     this.viewPortSubscription.unsubscribe();
   }
-
 }
