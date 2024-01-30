246fe8515d359a14358a9d1ade6b300a3730bf19
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-01-30 19:04:04 +0530

    reverted environment file changes
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- e2e/environment.ts
+++ e2e/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  HOST: '192.168.1.3',
+  HOST: 'localhost',
   SERVICE_PORT: '8081',
   AUTH_OPTIONAL: true
 };
--- e2e/po/app.skip.ts
+++ e2e/po/app.skip.ts
@@ -34,6 +34,7 @@ export class SkipLogin {
   }
 
   getAppStatus() {
+    cy.wait(2000); // slight delay before checking app status
     cy.get('app-navbar .navbar-menu .icon').should('be.visible')
   }
 
