d3f103a728abf6c2abd41fef8bbc0c05583c9659
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-01-30 17:34:58 +0530

    fixed failing text getAppStatus
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- e2e/po/app.skip.ts
+++ e2e/po/app.skip.ts
@@ -34,7 +34,7 @@ export class SkipLogin {
   }
 
   getAppStatus() {
-    cy.get('#app > app-root > div > app-navbar > nav > div.navbar-menu .icon').should('be.visible')
+    cy.get('app-navbar .navbar-menu .icon').should('be.visible')
   }
 
   getReceived() {
