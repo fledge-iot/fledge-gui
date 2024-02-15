b3c6c1498216b4cf0070c059070941a27c4adf12
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-02-15 12:21:03 +0530

    node size css fix
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/components/common/node-editor/add-service.ts
+++ src/app/components/common/node-editor/add-service.ts
@@ -2,11 +2,11 @@ import { ClassicPreset } from "rete";
 
 
 export class AddService extends ClassicPreset.Node {
-    height = 110;
-    width = 220;
-    parent?: string;
+  height = 110;
+  width = 220;
+  parent?: string;
 
-    constructor() {
-        super("AddService");
-    }
+  constructor() {
+    super("AddService");
+  }
 }
