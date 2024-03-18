233c73c2a8188c4796e52c5dae2bd8082fb24f3e
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-03-18 12:39:08 +0530

    fixed developer guard for the users don't have permission to access the page
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/guards/developer.guard.ts
+++ src/app/guards/developer.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { DeveloperFeaturesService } from '../services/developer-features.service';
+import { RolesGuard } from './roles.gurad';
+import { RolesService } from '../services';
 
 @Injectable()
-export class DeveloperGuard  {
-  constructor(private router: Router, public developerFeaturesService: DeveloperFeaturesService) { }
+export class DeveloperGuard {
+  constructor(
+    private router: Router,
+    public rolesService: RolesService,
+    public developerFeaturesService: DeveloperFeaturesService
+  ) { }
   canActivate() {
-    const isDeveloper = this.developerFeaturesService.getDeveloperFeatureControl();
-    if (isDeveloper) {
-      return true;
+    const canEdit = this.rolesService.hasEditPermissions();
+    if (canEdit) {
+      const isDeveloper = this.developerFeaturesService.getDeveloperFeatureControl();
+      if (isDeveloper) {
+        return true;
+      }
+      return false;
     }
     this.router.navigateByUrl('');
     return false;
