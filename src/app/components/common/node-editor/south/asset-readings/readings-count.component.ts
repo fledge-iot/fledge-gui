7cca6779a795b385df7a435cc2fc423c6e3cf839
hemant <hemantgarg923@gmail.com>
2024-03-21 14:37:49 +0530

    asset count updated while updating the node
    Signed-off-by: hemant <hemantgarg923@gmail.com>

--- src/app/components/common/node-editor/south/asset-readings/readings-count.component.ts
+++ src/app/components/common/node-editor/south/asset-readings/readings-count.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { AssetControl, ReadingControl } from '../../controls/south-custom-control';
 import { FlowEditorService } from '../../flow-editor.service';
 
 @Component({
@@ -8,15 +7,10 @@ import { FlowEditorService } from '../../flow-editor.service';
   styleUrls: ['./readings-count.component.css'],
 })
 export class ReadingsCountComponent {
-  @Input() readingCount: ReadingControl;
-  @Input() assetCount: AssetControl;
+  @Input() readingCount: string;
+  @Input() assetCount: string;
   @Input() serviceName;
-  readingsCount;
   constructor(public flowEditorService: FlowEditorService) { }
-  
-  ngOnInit(){
-    this.readingsCount = this.readingCount;
-  }
   
   showReadingsPerAsset() {
     this.flowEditorService.showItemsInQuickview.next({ showReadings: true, serviceName: this.serviceName });
