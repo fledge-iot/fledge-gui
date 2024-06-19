61c9caf645d103af8fde4bd48c8a3b8b400dda16
monika-Dianomic <monika@dianomic.com>
2024-06-19 13:29:40 +0530

    initial commit

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -1,11 +1,38 @@
 import { Pipe, PipeTransform } from '@angular/core';
+
 @Pipe({
   name: 'SearchPipe'
 })
 export class SearchPipe implements PipeTransform {
   transform(items: any, searchText: any): any {
     if (searchText) {
-      searchText = searchText.toLowerCase();
+      console.log('search', searchText);
+      if (searchText instanceof Array) {
+        let filteredData = [];
+        for (const text of searchText) {
+          console.log('TEXT', text);
+          const data = this.filterData(items, text);
+          filteredData.push(data);
+        }
+        items = filteredData;
+      } else {
+        this.filterData(items, searchText);
+      }
+      // this.filterData(items, searchText);
+      // searchText = searchText.toLowerCase();
+      // return items.filter((item: any) => {
+      //   if (items && items[0].details) {
+      //     return item.details.name.toLowerCase().indexOf(searchText) > -1;
+      //   } else {
+      //     return item.toLowerCase().indexOf(searchText) > -1;
+      //   }
+      // });
+    }
+    return items;
+  }
+
+  filterData(items: any, searchText: any) {
+    searchText = searchText.toLowerCase();
       return items.filter((item: any) => {
         if (items && items[0].details) {
           return item.details.name.toLowerCase().indexOf(searchText) > -1;
@@ -13,7 +40,5 @@ export class SearchPipe implements PipeTransform {
           return item.toLowerCase().indexOf(searchText) > -1;
         }
       });
-    }
-    return items;
   }
 }
