f53877a23040c874407cbfe7beb8fc587b535fdd
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-07-10 15:14:18 +0530

    moved static data in separate component
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

---
+++ src/app/components/core/configuration-manager/static-configuration/static-configuration.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+
+@Component({
+  standalone: true,
+  selector: 'app-static-configuration',
+  templateUrl: './static-configuration.component.html',
+  styleUrls: ['./static-configuration.component.css'],
+  imports: [CommonModule, CodemirrorModule, FormsModule]
+})
+export class StaticConfigurationComponent {
+  @Input() configuration;
+  constructor() { }
+
+  public codeMirrorConfiguration(type: string) {
+    const editorOptions = {
+      mode: 'text/x-python',
+      lineNumbers: true,
+      lineWrapping: true,
+      foldGutter: true,
+      gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter', 'CodeMirror-lint-markers'],
+      autoCloseBrackets: true,
+      matchBrackets: true,
+      lint: true,
+      inputStyle: 'textarea',
+      autoRefresh: true
+    };
+    if (type === 'JSON') {
+      editorOptions.mode = 'application/json';
+    }
+    return editorOptions;
+  }
+}
