57f13da2a91217168c33e298c02b2a73cb608b7d
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2024-09-05 18:49:43 +0530

    FOFL-8986: Filter node change in dock menu  (#452)
    * Fixed filter drop issue on the canvas while we have filter place holder node already available on the canvas
    * Fixed add filter node + icon issue and hidding node connection in dock menu
    * updated alert class to show autonomous message for invalid filter node

--- src/app/components/common/node-editor/connector.ts
+++ src/app/components/common/node-editor/connector.ts
@@ -1,6 +1,7 @@
 import { ClassicPreset, GetSchemes, getUID } from 'rete';
 import { BidirectFlow, Context, SocketData } from 'rete-connection-plugin';
 import { getUpdatedFilterPipeline } from './editor';
+import { PseudoNodeControl } from './filter';
 
 type ClassicScheme = GetSchemes<ClassicPreset.Node, ClassicPreset.Connection<ClassicPreset.Node, ClassicPreset.Node> & { isLoop?: boolean }>
 
@@ -12,8 +13,11 @@ export class Connector<S extends ClassicScheme, K extends any[]> extends Bidirec
         if (initial.nodeId === socket.nodeId) {
           return;
         }
-        // Avoid connection loop in pipeline
+
+        const fromNode = context.editor.getNode(initial.nodeId);
         const toNode = context.editor.getNode(socket.nodeId);
+
+        // Avoid connection loop in pipeline
         const pipeline = getUpdatedFilterPipeline();
         let exists = false;
         if (typeof pipeline == 'object') {
@@ -33,11 +37,35 @@ export class Connector<S extends ClassicScheme, K extends any[]> extends Bidirec
             isLoop: false,
             ...props
           })
-        return true
+
+        // To hide/show (+) icon on the add filter node
+        setTimeout(() => {
+          const connections = context.editor.getConnections();
+          if (fromNode.label == 'Filter') {
+            const inputConnection = connections.find(conn => (fromNode.id == conn.source));
+            const outputConnection = connections.find(conn => ((fromNode.id == conn.target)));
+            if (inputConnection && outputConnection) {
+              const pseudoNodeControl = fromNode.controls.pseudoNodeControl as PseudoNodeControl;
+              pseudoNodeControl.pseudoConnection = true;
+            }
+          }
+
+          if (toNode.label == 'Filter') {
+            const inputConnection = connections.find(conn => (toNode.id == conn.source));
+            const outputConnection = connections.find(conn => ((toNode.id == conn.target)));
+            if (inputConnection && outputConnection) {
+              const pseudoNodeControl = toNode.controls.pseudoNodeControl as PseudoNodeControl;
+              pseudoNodeControl.pseudoConnection = true;
+            }
+          }
+        }, 0);
+
+        return true;
       }
     })
   }
 }
+
 
 export function contains(item: any, pipeline: any[]): boolean {
   // check element in filter pipeline
