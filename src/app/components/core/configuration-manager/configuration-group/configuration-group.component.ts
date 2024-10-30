575294b5905e40cb447b28d8ccebfce724c91c39
monika-Dianomic <monika@dianomic.com>
2024-10-30 16:09:17 +0530

    fix for large screen size

--- src/app/components/core/configuration-manager/configuration-group/configuration-group.component.ts
+++ src/app/components/core/configuration-manager/configuration-group/configuration-group.component.ts
@@ -55,14 +55,10 @@ export class ConfigurationGroupComponent implements AfterViewInit {
     if (this.from.includes("control-pipeline")) {
       const element = document.getElementById(this.from);
       if (element) {
-        const currentMaxWidth = window.getComputedStyle(element).maxWidth;
-        const currentMaxWidthValue = parseFloat(currentMaxWidth);
+        const currentMaxWidthValue = parseFloat(window.getComputedStyle(element).maxWidth);
 
-        // Calculate 65% of the current max-width
-        const newMaxWidthValue = currentMaxWidthValue * 0.65;
-
-        // Set the new max-width (keeping it in px)
-        element.style.maxWidth = newMaxWidthValue + 'px';
+        // Set the new max-width (65% of the current max-width)
+        element.style.maxWidth = currentMaxWidthValue * 0.65 + 'px';
       }
     }
     const idSuffix = this.from + '_' + this.sourceName;
@@ -89,9 +85,7 @@ export class ConfigurationGroupComponent implements AfterViewInit {
   ngOnChanges() {
     this.categeryConfiguration();
     this.getChildConfigData();
-    if (this.isFilterList && this.recalculateTabsOverflow !== undefined) {
-      this.tabs.setOverFlow();
-    } else if (this.recalculateTabsOverflow) {
+    if ((this.isFilterList && this.recalculateTabsOverflow !== undefined) || this.recalculateTabsOverflow) {
       this.tabs.setOverFlow();
     }
   }
