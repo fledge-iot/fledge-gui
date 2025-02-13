852d00f114ec9c39745548333f335d53c18a722b
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2025-02-13 11:42:37 +0530

    fixed task undefine issue
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/components/common/node-editor/node-editor.component.ts
+++ src/app/components/common/node-editor/node-editor.component.ts
@@ -531,7 +531,9 @@ export class NodeEditorComponent implements OnInit {
       .subscribe((data: any) => {
         const tasks = data as NorthTask[];
         this.tasks = tasks;
-        this.task = tasks.find(t => (t.name == this.source));
+        if (this.source) {
+          this.task = tasks.find(t => (t.name == this.source));
+        }
 
         data = {
           from: this.from,
