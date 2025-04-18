1bc4da99f3be210dc46c5dc5ba12416f21da972f
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2025-04-18 18:36:51 +0530

    Added debug icons on the south & north list page for the service that have the debugger attached
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { DateFormatterPipe, FilterPipe, KeysPipe, SearchPipe, TruncatePipe, TruncateMiddleOfTextPipe } from '.';
+import { HasAttachedDebuggerPipe } from '../components/core/debugger/has-attached-debugger.pipe';
 
 @NgModule({
-  declarations: [DateFormatterPipe, KeysPipe, FilterPipe, SearchPipe, TruncatePipe, TruncateMiddleOfTextPipe],
-  exports: [DateFormatterPipe, KeysPipe, FilterPipe, SearchPipe, TruncatePipe, TruncateMiddleOfTextPipe]
+  declarations: [DateFormatterPipe, KeysPipe, FilterPipe, SearchPipe, TruncatePipe, TruncateMiddleOfTextPipe, HasAttachedDebuggerPipe],
+  exports: [DateFormatterPipe, KeysPipe, FilterPipe, SearchPipe, TruncatePipe, TruncateMiddleOfTextPipe, HasAttachedDebuggerPipe]
 })
 export class PipesModule { }
