7a4375b446b647a388a8c7d2fa170251bf1bf902
monika-Dianomic <monika@dianomic.com>
2025-04-21 15:56:32 +0530

    updated condition to check null value

--- src/app/components/common/node-editor/connector.ts
+++ src/app/components/common/node-editor/connector.ts
@@ -53,7 +53,7 @@ export class Connector<S extends ClassicScheme, K extends any[]> extends Bidirec
         // Avoid connection loop in pipeline
         const updatedPipeline = getUpdatedFilterPipeline();
         let exists = false;
-        if (typeof updatedPipeline == 'object') {
+        if (updatedPipeline && typeof updatedPipeline == 'object') {
           exists = contains(toNode.label, updatedPipeline);
           if (exists) {
             alertService.error('Joining branches in a pipeline is not supported', true);
