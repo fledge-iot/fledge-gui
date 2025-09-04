1ac0177b55964a4c2e1e29c38a1851ea5b27a648
Mohd. Shariq <mohd.shariq@nerdapplabs.com>
2025-09-04 20:40:17 +0530

    UI improvement for plugin validation report
    Signed-off-by: Mohd. Shariq <mohd.shariq@nerdapplabs.com>

--- src/app/components/common/validation-report/validation-report.component.ts
+++ src/app/components/common/validation-report/validation-report.component.ts
@@ -1,31 +1,39 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 
 @Component({
-    selector: 'app-validation-report',
-    templateUrl: './validation-report.component.html',
-    styleUrls: ['./validation-report.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-validation-report',
+  templateUrl: './validation-report.component.html',
+  styleUrls: ['./validation-report.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ValidationReportComponent {
-    @Input() results: any;
-    @Input() loading: boolean = false;
-    @Input() error: string | null = null;
-    @Output() close = new EventEmitter<void>();
+  @Input() results: any;
+  @Input() loading: boolean = false;
+  @Input() error: string | null = null;
+  @Output() close = new EventEmitter<void>();
 
-    public expandedTests: { [key: string]: boolean } = {};
-    public minimized = false;
-
-    public toggleReport(key: string) {
-        this.expandedTests[key] = !this.expandedTests[key];
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes['results']) {
+      console.log(this.results);
+      this.results = changes['results'].currentValue;
     }
+  }
 
-    public toggleMinimize() {
-        this.minimized = !this.minimized;
-    }
+  public expandedTests: { [key: string]: boolean } = {};
+  public minimized = false;
 
-    public onClose() {
-        this.close.emit();
-    }
+  public toggleReport(key: string) {
+    this.expandedTests[key] = !this.expandedTests[key];
+  }
+
+  public toggleMinimize() {
+    this.minimized = !this.minimized;
+  }
+
+  public onClose() {
+    this.close.emit();
+  }
 }
 
 
